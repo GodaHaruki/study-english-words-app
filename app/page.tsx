300e39635274d233c9899fecaaa0252740dcf318
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2024-10-08 01:26:04 +0000

    fix lint error
    escape single quote

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
       <h2 className="mb-0 text-3xl col-span-12 text-center bg-gradient-to-r from-primary to-primary-content bg-clip-text text-transparent">日本語→英語 でスペルも確認</h2>
       <h2 className="mb-0 text-3xl col-span-12 text-center bg-gradient-to-r from-primary to-primary-content bg-clip-text text-transparent">単語帳を作成して復習</h2>
       <h2 className="mb-5 mt-0 text-3xl col-span-12 text-center bg-gradient-to-r from-primary to-primary-content bg-clip-text text-transparent">無料で英単語を学ぼう</h2>
-      <Link href="./study" className="col-span-12 mx-auto btn btn-wide btn-primary text-xl">Let's start!</Link>
+      <Link href="./study" className="col-span-12 mx-auto btn btn-wide btn-primary text-xl">Let&apos;s start!</Link>
       <div className="col-span-12 grid grid-cols-12">
         <div className="col-span-10 col-start-2 divider"/>
       </div>
