ee76f78c94996d055f099d9cd3bbf32d3eb7fba1
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-12 17:44:52 +0000

    change some coding style

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -4,25 +4,26 @@ import Footer from '@/components/Footer';
 import Link from 'next/link';
 
 export function CustomCard(props: {
-  class: string;
+  className: string;
   link: string;
   title: string;
   desc: string;
-  titleClass?: string;
-  descClass?: string;
+  titleClassName?: string;
+  descClassName?: string;
 }) {
   return (
     <Link
       href={props.link}
       className={
-        props.class + ' card card-normal col-span-10 col-start-2 no-underline'
+        props.className +
+        ' card card-normal col-span-10 col-start-2 no-underline'
       }
     >
       <div className='card-body col-span-10'>
-        <h2 className={props.titleClass + ' card-title text-3xl'}>
+        <h2 className={props.titleClassName + ' card-title text-3xl'}>
           {props.title}
         </h2>
-        <p className={props.descClass + ' card-text'}>{props.desc}</p>
+        <p className={props.descClassName + ' card-text'}>{props.desc}</p>
       </div>
     </Link>
   );
@@ -35,24 +36,29 @@ export default function Study() {
       <h2 className='col-span-12 mb-0 mt-5 bg-gradient-to-r from-primary to-primary-content bg-clip-text text-center text-3xl text-transparent'>
         学習方法を選択してください
       </h2>
-      <CustomCard
-        class='rounded-lg bg-slate-100'
-        link='./study/e2j'
-        title='英語を日本語に'
-        desc='英単語の意味を回答する問題です'
-      />
-      <CustomCard
-        class='rounded-lg bg-slate-100'
-        link='./study/j2e'
-        title='日本語を英語に'
-        desc='日本語の意味を回答する問題です'
-      />
-      <CustomCard
-        class='rounded-lg bg-slate-100'
-        link='./study/dict'
-        title='単語帳'
-        desc='訳や例文などが載っている単語帳です'
-      />
+      {[
+        {
+          link: './study/e2j',
+          title: '英語を日本語に',
+          desc: '英単語の意味を回答する問題です',
+        },
+        {
+          link: './study/j2e',
+          title: '日本語を英語に',
+          desc: '日本語の意味を回答する問題です',
+        },
+        {
+          link: './study/dict',
+          title: '単語帳',
+          desc: '訳や例文などが載っている単語帳です',
+        },
+      ].map((props) => (
+        <CustomCard
+          className='rounded-lg bg-slate-100'
+          key={props.title}
+          {...props}
+        />
+      ))}
       <Footer />
     </div>
   );
