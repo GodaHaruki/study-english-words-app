cd0a0ba4401ef1b99fb07705026c15b0cc30722a
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-14 20:25:52 +0000

    npm run format

--- hooks/search-word/hooks.ts
+++ hooks/search-word/hooks.ts
@@ -1,22 +1,26 @@
-import { useEffect, useState } from "react"
-import init, { get_word_distances_sorted } from "./pkg"
+import { useEffect, useState } from 'react';
+import init, { get_word_distances_sorted } from './pkg';
 
 /*
 CSV should follow RFC4180 Format
 https://www.rfc-editor.org/rfc/rfc4180.txt
 */
 export const useSearchWord = (CSVPath: string) => {
-  const [csv, setCSV] = useState<string>("");
+  const [csv, setCSV] = useState<string>('');
   useEffect(() => {
-    fetch(CSVPath).then(v => v.text()).then(s => setCSV(s));
-    init()
-  }, [])
+    fetch(CSVPath)
+      .then((v) => v.text())
+      .then((s) => setCSV(s));
+    init();
+  }, []);
 
   const searchByWord = (q: string) => {
     const { words, distances } = get_word_distances_sorted(q, csv);
 
-    return words.map((w: string, i: number) => { return { word: w, distance: distances[i] } })
-  }
+    return words.map((w: string, i: number) => {
+      return { word: w, distance: distances[i] };
+    });
+  };
 
-  return { searchByWord }
-}
+  return { searchByWord };
+};
