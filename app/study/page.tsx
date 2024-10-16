f5a8c7309c0bddf6e79de13fc67910fd8c989808
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-16 13:18:34 +0000

    fix next build error

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Link from 'next/link';
 
-export function CustomCard(props: {
+function CustomCard(props: {
   className: string;
   link: string;
   title: string;
