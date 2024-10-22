abca2724f9a6f6641beafe082dff3699aa52e1c1
maguro923 <maguro29y@gmail.com>
2024-10-22 13:39:18 +0000

    fix lint and format error

--- app/study/[studyType]/page.tsx
+++ app/study/[studyType]/page.tsx
@@ -3,18 +3,23 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Link from 'next/link';
 
-const studyTypeText = {'e2j':"英語を日本語に", 'j2e':"日本語を英語に", 'dict':"単語帳"};
+const studyTypeText = {
+  e2j: '英語を日本語に',
+  j2e: '日本語を英語に',
+  dict: '単語帳',
+};
 
-
-function Title(props: { type: "e2j"|"j2e"|"dict" }) {
+function Title(props: { type: 'e2j' | 'j2e' | 'dict' }) {
   return (
-    <div className={grid+' col-span-12'}>
-      <p className='text-center text-3xl col-span-4 my-5'>
+    <div className={grid + ' col-span-12'}>
+      <p className='col-span-4 my-5 text-center text-3xl'>
         <Link href='/study' className='text-inherit no-underline'>
           戻る
         </Link>
       </p>
-      <p className='text-center text-3xl col-span-8 md:col-span-4 my-5'>{studyTypeText[props.type]}</p>
+      <p className='col-span-8 my-5 text-center text-3xl md:col-span-4'>
+        {studyTypeText[props.type]}
+      </p>
     </div>
   );
 }
@@ -37,21 +42,26 @@ export default function Page({
 }: {
   params: { studyType: (typeof StudyType)[keyof typeof StudyType] };
 }) {
-  
   const { studyType } = params;
 
-  const selectItemJSX:{
+  const selectItemJSX: {
     e2j: JSX.Element[];
     j2e: JSX.Element[];
     dict: JSX.Element[];
   } = {
-    "e2j": [
-      <label className={'grid grid-cols-12 col-span-12 text-center text-2xl mt-6'}>
+    e2j: [
+      <label
+        key='1'
+        className={'col-span-12 mt-6 grid grid-cols-12 text-center text-2xl'}
+      >
         <div className='col-span-6 flex items-center justify-center'>
           <p className='mx-2 my-0'>出題範囲</p>
         </div>
         <div className='col-span-6'>
-          <select name='range' className='select select-bordered self-center w-5/6 my-0'>
+          <select
+            name='range'
+            className='select select-bordered my-0 w-5/6 self-center'
+          >
             <option value='all'>全て</option>
             <option value='ngsl'>NGSL</option>
             <option value='nawl'>NAWL</option>
@@ -61,25 +71,38 @@ export default function Page({
           </select>
         </div>
       </label>,
-      <label className={'grid grid-cols-12 col-span-12 text-center text-2xl mt-6'}>
+      <label
+        key='2'
+        className={'col-span-12 mt-6 grid grid-cols-12 text-center text-2xl'}
+      >
         <div className='col-span-6 flex items-center justify-center'>
           <p className='mx-2 my-0'>出題数</p>
         </div>
         <div className='col-span-6'>
-          <select name='num' className='select select-bordered self-center w-5/6 my-0'>
+          <select
+            name='num'
+            className='select select-bordered my-0 w-5/6 self-center'
+          >
             <option value='10'>10</option>
             <option value='20'>20</option>
             <option value='30'>30</option>
           </select>
         </div>
-      </label>],
-    "j2e": [
-      <label className={'grid grid-cols-12 col-span-12 text-center text-2xl mt-6'}>
+      </label>,
+    ],
+    j2e: [
+      <label
+        key='1'
+        className={'col-span-12 mt-6 grid grid-cols-12 text-center text-2xl'}
+      >
         <div className='col-span-6 flex items-center justify-center'>
           <p className='mx-2 my-0'>出題範囲</p>
         </div>
         <div className='col-span-6'>
-          <select name='range' className='select select-bordered self-center w-5/6 my-0'>
+          <select
+            name='range'
+            className='select select-bordered my-0 w-5/6 self-center'
+          >
             <option value='all'>全て</option>
             <option value='ngsl'>NGSL</option>
             <option value='nawl'>NAWL</option>
@@ -89,25 +112,38 @@ export default function Page({
           </select>
         </div>
       </label>,
-      <label className={'grid grid-cols-12 col-span-12 text-center text-2xl mt-6'}>
+      <label
+        key='2'
+        className={'col-span-12 mt-6 grid grid-cols-12 text-center text-2xl'}
+      >
         <div className='col-span-6 flex items-center justify-center'>
           <p className='mx-2 my-0'>出題数</p>
         </div>
         <div className='col-span-6'>
-          <select name='num' className='select select-bordered self-center w-5/6 my-0'>
+          <select
+            name='num'
+            className='select select-bordered my-0 w-5/6 self-center'
+          >
             <option value='10'>10</option>
             <option value='20'>20</option>
             <option value='30'>30</option>
           </select>
         </div>
-      </label>],
-    "dict": [
-      <label className={'grid grid-cols-12 col-span-12 text-center text-2xl mt-6'}>
+      </label>,
+    ],
+    dict: [
+      <label
+        key='1'
+        className={'col-span-12 mt-6 grid grid-cols-12 text-center text-2xl'}
+      >
         <div className='col-span-6 flex items-center justify-center'>
           <p className='mx-2 my-0'>出題範囲</p>
         </div>
         <div className='col-span-6'>
-          <select name='range' className='select select-bordered self-center w-5/6 my-0'>
+          <select
+            name='range'
+            className='select select-bordered my-0 w-5/6 self-center'
+          >
             <option value='all'>全て</option>
             <option value='ngsl'>NGSL</option>
             <option value='nawl'>NAWL</option>
@@ -117,28 +153,40 @@ export default function Page({
           </select>
         </div>
       </label>,
-      <label className={'grid grid-cols-12 col-span-12 text-center text-2xl mt-6'}>
+      <label
+        key='2'
+        className={'col-span-12 mt-6 grid grid-cols-12 text-center text-2xl'}
+      >
         <div className='col-span-6 flex items-center justify-center'>
           <p className='mx-2 my-0'>出題数</p>
         </div>
         <div className='col-span-6'>
-          <select name='num' className='select select-bordered self-center w-5/6 my-0'>
+          <select
+            name='num'
+            className='select select-bordered my-0 w-5/6 self-center'
+          >
             <option value='10'>10</option>
             <option value='20'>20</option>
             <option value='30'>30</option>
           </select>
         </div>
-      </label>]
+      </label>,
+    ],
   };
 
   return (
     <div className={grid}>
       <Header />
-      <Title type={studyType}/>
+      <Title type={studyType} />
       <div className={'col-span-12 mx-5'}>
-        <form className='grid grid-cols-12 col-span-12 bg-slate-100 rounded-lg'>
+        <form className='col-span-12 grid grid-cols-12 rounded-lg bg-slate-100'>
           {selectItemJSX[studyType].map((item) => item)}
-          <button type='submit' className='btn btn-primary col-start-3 col-span-8 self-center mt-6 mb-3'>スタート</button>
+          <button
+            type='submit'
+            className='btn btn-primary col-span-8 col-start-3 mb-3 mt-6 self-center'
+          >
+            スタート
+          </button>
         </form>
       </div>
       <Footer />
